96922ed2fe8db10b60585d879620d01fe9b33512
irineu silva <mmmirineusilva@gmail.com>
2023-09-18 01:59:20 -0300

    feat: implement encode to string function

--- src/commonMain/kotlin/com/neo/base64/Base64Encoder.kt
+++ src/commonMain/kotlin/com/neo/base64/Base64Encoder.kt
@@ -2,4 +2,12 @@ package com.neo.base64
 
 interface Base64Encoder {
     fun encode(src: ByteArray): ByteArray
+
+    fun encodeToString(src: ByteArray): String {
+        val encoded = encode(src)
+
+        return buildString(encoded.size) {
+            encoded.forEach { append(it.toInt().toChar()) }
+        }
+    }
 }
--- src/jvmMain/kotlin/com/neo/base64/JvmBase64Encoder.kt
+++ src/jvmMain/kotlin/com/neo/base64/JvmBase64Encoder.kt
@@ -4,4 +4,7 @@ import java.util.Base64
 
 object JvmBase64Encoder : Base64Encoder {
     override fun encode(src: ByteArray): ByteArray = Base64.getEncoder().encode(src)
+
+    // more efficient implementation in jvm
+    override fun encodeToString(src: ByteArray): String = Base64.getEncoder().encodeToString(src)
 }

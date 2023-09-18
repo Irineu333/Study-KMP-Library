ae2b3f1104191d3fed3d1583854d1c0e6f344c90
irineu silva <mmmirineusilva@gmail.com>
2023-09-18 01:43:24 -0300

    fix: missing native target in the build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform") version "1.9.0"
 }
@@ -31,7 +34,13 @@ kotlin {
         }
     }
 
+    // don't remove it, it's important
+    val nativeTarget = getNativeTarget()
+
     sourceSets {
+
+        // get source set by property name
+
         val commonMain by getting
         val commonTest by getting {
             dependencies {
@@ -51,5 +60,20 @@ kotlin {
 
         val nativeMain by getting
         val nativeTest by getting
+    }
+}
+
+fun KotlinMultiplatformExtension.getNativeTarget() : KotlinNativeTarget {
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
+
+   return when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 }
